54c071eb4d1c071765a645dadb1b32b7e2e8160c
MortenStaunstrup <mort490fgromling@gmail.com>
2026-05-11 10:54:11 +0200

    Added project versioning

--- AuthAPI/Controllers/AuthController.cs
+++ AuthAPI/Controllers/AuthController.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using AuthAPI.Models;
 using AuthAPI.Token;
 using Microsoft.AspNetCore.Mvc;
@@ -81,5 +82,26 @@ public class AuthController : ControllerBase
         }
         _logger.LogInformation($"Returning JWT token for Admin with id: {admin.AdminId}");
         return Ok(tokenString);
+    }
+    
+    [HttpGet("version")]
+    public async Task<Dictionary<string,string>> GetVersion()
+    {
+        var properties = new Dictionary<string, string>();
+        var assembly = typeof(Program).Assembly;
+        properties.Add("service", "FitLife Auth service");
+        var ver = FileVersionInfo.GetVersionInfo(typeof(Program)
+            .Assembly.Location).ProductVersion;
+        properties.Add("version", ver!);
+        try {
+            var hostName = System.Net.Dns.GetHostName();
+            var ips = await System.Net.Dns.GetHostAddressesAsync(hostName);
+            var ipa = ips.First().MapToIPv4().ToString();
+            properties.Add("hosted-at-address", ipa);
+        } catch (Exception ex) {
+            _logger.LogError(ex.Message);
+            properties.Add("hosted-at-address", "Could not resolve IP-address");
+        }
+        return properties;
     }
 }
